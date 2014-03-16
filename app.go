c49e4b3c82ffed85d5da339d8af9b991a29d9537
Chad Kouse <chad.kouse@gmail.com>
2014-03-16 16:53:45 -0400

    GPIO integration

--- app.go
+++ app.go
@@ -3,12 +3,15 @@ package main
 import (
 	"encoding/binary"
 	"encoding/json"
+	"fmt"
 	"github.com/ant0ine/go-json-rest"
 	"github.com/davecheney/gpio"
+	"github.com/davecheney/gpio/rpi"
 	"github.com/steveyen/gkvlite"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"sethwklein.net/go/errutil"
 	"time"
 )
@@ -31,24 +34,35 @@ type Flag struct {
 	Value int
 }
 
+type Sensor struct {
+	Name string
+	Pin  int
+}
+
+type SystemStatus struct {
+	Status     string
+	LastUpdate time.Time
+}
+
 var f os.File
+var sensors []Sensor
 var s gkvlite.Store
+var sysStatus SystemStatus
 var dbname = "/tmp/test.db"
 
 func setObj(collection string, key []byte, object interface{}) (err error) {
 	f2, er := os.OpenFile(dbname, os.O_RDWR, 0666)
 	defer errutil.AppendCall(&err, f2.Close)
-	defer errutil.AppendCall(&err, f2.Sync)
 	if er != nil {
 		log.Println("Error while opening db", er)
 		return er
 	}
 	s2, er := gkvlite.NewStore(f2)
-	defer errutil.AppendCall(&err, s2.Flush)
 	if er != nil {
 		log.Println("Error while opening store", er)
 		return er
 	}
+	defer errutil.AppendCall(&err, s2.Flush)
 
 	c := s2.SetCollection(collection, nil)
 
@@ -81,37 +95,37 @@ func AddEvent(e Event) error {
 	return setObj("events", b, e)
 }
 
-func GetAllEvents() (error, []Event) {
+func GetAllEvents() (err error, events []Event) {
 	log.Println("getting all events")
-	f2, err := os.Open(dbname)
-	if err != nil || f2 == nil {
-		log.Println("Error while opening db", err)
-		return err, nil
+	f2, er := os.Open(dbname)
+	defer errutil.AppendCall(&err, f2.Close)
+	if er != nil || f2 == nil {
+		log.Println("eror while opening db", er)
+		return er, events
 	}
-	s2, err := gkvlite.NewStore(f2)
-	if err != nil {
-		log.Println("Error while opening store", err)
-		return err, nil
+	s2, er := gkvlite.NewStore(f2)
+	if er != nil {
+		log.Println("eror while opening store", er)
+		return er, events
 	}
 
 	c := s2.GetCollection("events")
-	maxItem, err := c.MaxItem(true)
-	if err != nil {
-		return err, nil
+	maxItem, er := c.MaxItem(true)
+	if er != nil {
+		return er, events
 	}
-	events := []Event{}
 	if maxItem == nil {
 		return nil, events
 	}
 	j := Event{}
-	err = json.Unmarshal(maxItem.Val, &j)
+	er = json.Unmarshal(maxItem.Val, &j)
 	events = append(events, j)
 	log.Println("Here", j)
-	err = c.VisitItemsDescend(maxItem.Key, true, func(i *gkvlite.Item) bool {
+	er = c.VisitItemsDescend(maxItem.Key, true, func(i *gkvlite.Item) bool {
 		j := Event{}
-		err := json.Unmarshal(i.Val, &j)
-		if err != nil {
-			log.Println("Error unmarshalling value", err)
+		er := json.Unmarshal(i.Val, &j)
+		if er != nil {
+			log.Println("eror unmarshalling value", er)
 		} else {
 			events = append(events, j)
 		}
@@ -119,11 +133,12 @@ func GetAllEvents() (error, []Event) {
 		return true
 	})
 
-	if err != nil {
-		log.Println("Error scanning table 'events'", err)
+	if er != nil {
+		log.Println("eror scanning table 'events'", er)
+		return er, events
 	}
 
-	return nil, events
+	return err, events
 }
 
 func ArmSystem() error {
@@ -157,18 +172,31 @@ func HandleGetAllEvents(w *rest.ResponseWriter, req *rest.Request) {
 
 }
 
+func handleGetStatus(w *rest.ResponseWriter, req *rest.Request) {
+	w.WriteJson(sysStatus)
+}
+
 func GetUser(w *rest.ResponseWriter, req *rest.Request) {
-	f2, err := os.Open(dbname)
+	f2, er := os.Open(dbname)
+	if er != nil {
+		log.Fatal("Error opening database", er)
+		return
+	}
+
 	defer f2.Close()
-	s2, err := gkvlite.NewStore(f2)
-	c := s2.SetCollection("users", nil)
-	//time.Now().Unix()
+
+	s2, er := gkvlite.NewStore(f2)
+	if er != nil {
+		log.Fatal("Error opening store", er)
+		return
+	}
+	c := s2.GetCollection("users")
 
 	var user User
 
-	userBytes, err := c.Get([]byte(req.PathParam("id")))
-	if err != nil {
-		log.Fatal(err)
+	userBytes, er := c.Get([]byte(req.PathParam("id")))
+	if er != nil {
+		log.Fatal(er)
 		return
 	}
 	if userBytes == nil {
@@ -176,16 +204,98 @@ func GetUser(w *rest.ResponseWriter, req *rest.Request) {
 		return
 	}
 
-	err = json.Unmarshal(userBytes, &user)
-	if err != nil {
-		log.Fatal(err)
+	er = json.Unmarshal(userBytes, &user)
+	if er != nil {
+		log.Fatal(er)
 		return
 	}
 
 	w.WriteJson(&user)
+	return
 
 }
+
+func updateSystemStatus(newStatus string) {
+	sysStatus = SystemStatus{
+		Status:     newStatus,
+		LastUpdate: time.Now(),
+	}
+}
+
+func watchSensor(sensor Sensor) {
+	pin, err := rpi.OpenPin(sensor.Pin, gpio.ModeInput)
+	fmt.Printf("opening pin for %s (%d)!\n", sensor.Name, sensor.Pin)
+	if err != nil {
+		fmt.Printf("Error opening pin for %s (%d)! %s\n", sensor.Name, sensor.Pin, err)
+		return
+	}
+
+	err = pin.BeginWatch(gpio.EdgeBoth, func() {
+		if pin.Get() {
+			fmt.Printf("OPEN for %s (%d) triggered!\n\n", sensor.Name, sensor.Pin)
+		} else {
+			fmt.Printf("CLOSE for %s (%d) triggered!\n\n", sensor.Name, sensor.Pin)
+		}
+	})
+	if err != nil {
+		fmt.Printf("Unable to watch pin: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			fmt.Println("Closing pin and terminating program.")
+			pin.Close()
+		}
+	}()
+}
+
+func setupGPIO() {
+
+	for _, sensor := range sensors {
+		go watchSensor(sensor)
+	}
+
+	//blink
+	power, err := gpio.OpenPin(rpi.GPIO17, gpio.ModeOutput)
+	if err != nil {
+		fmt.Printf("Error opening pin! %s\n", err)
+		return
+	}
+
+	// clean up on exit
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			fmt.Println("Closing blink and terminating program.")
+			power.Clear()
+			power.Close()
+			os.Exit(0)
+		}
+	}()
+
+	fmt.Println("Now watching pin 22 on a falling edge.")
+	updateSystemStatus("Ready")
+
+	for {
+		// log.Println("Setting power high")
+		power.Set()
+		time.Sleep(2000 * time.Millisecond)
+		// log.Println("Setting power low")
+		power.Clear()
+		time.Sleep(2000 * time.Millisecond)
+	}
+}
+
 func main() {
+	updateSystemStatus("Unknown")
+
+	sensors = append(sensors, Sensor{Name: "Front Door", Pin: rpi.GPIO22})
+
+	setupGPIO()
 	name := dbname
 	f, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0660)
 	if err != nil {
@@ -198,7 +308,6 @@ func main() {
 	s, err := gkvlite.NewStore(f)
 
 	s.Flush()
-	f.Sync()
 	f.Close()
 
 	e := Event{}
